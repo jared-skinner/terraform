a1c65f243adb803396536b1248a4f10043bc0f8f
Jared Skinner <jaredds66@gmail.com>
2015-10-07 21:56:34 -0500

    started install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#install programs! its a start...
+
+pacman -S mutt vim tmux inkscape sudo 
+
+
+#symlink files!
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vimrc ~/.gitconfig
+ln -sf ~/dotfiles/.vimrc ~/.Xdefaults
+ln -sf ~/dotfiles/.vimrc ~/.zshrc
+ln -sf ~/dotfiles/.vimrc ~/.muttrc
