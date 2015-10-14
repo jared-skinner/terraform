d2a42cbfa4e6f761d3200c81e8debc946c1e2698
Jared Skinner <jaredds66@gmail.com>
2015-10-13 20:30:38 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,12 +1,32 @@
-#install programs! its a start...
+#install yaourt
+mkdir ~/Builds
+cd ~/Builds
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz
+tar -xzvf yaourt.tar.gz
+cd yaourt
 
-pacman -S mutt vim tmux inkscape sudo 
-
+#install stuff!
+pacman -S mutt							#terminal email client
+pacman -S vim							#text editor
+pacman -S tmux							#terminal multiplexer
+pacman -S sudo							#temporarily grant admin privilages
+yaourt extra/msmtp						#email client
+yaourt core/ca-certificates				#certificates
+yaourt local/thinkpad-scripts			#thinkpad scripts
+yaourt extra/keychain					#password management
+yaourt extra/xclip						#graphical clipboard
+yaourt community/pass					#password management?
+yaourt community/pwgen					#password generation
+yaourt extra/tree						#cool way of displaying directory structure
+yaourt community/the_silver_searcher	#like awk but better and faster
+yaourt extra/evince						#my pdf reader of choice
+yaourt extra/gimp						#Gnu Image Manipulation Program
+yaourt extra/inkscape					#vector editor
 
 #symlink files!
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vimrc ~/.gitconfig
-ln -sf ~/dotfiles/.vimrc ~/.Xdefaults
-ln -sf ~/dotfiles/.vimrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.muttrc
+ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/X/Xdefaults ~/.Xdefaults
+ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/mutt/.muttrc ~/.muttrc
